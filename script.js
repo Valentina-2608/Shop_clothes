f6f0288c419a5d1c322ff117b5612f0e52b27930
Valetina-2608 <valusha2608@gmail.com>
2023-01-07 10:53:46 +0200

    Add functions for show and hide goods

--- script.js
+++ script.js
@@ -36,3 +36,90 @@ function load(){
 	 	createCard(good, good.image, good.caption, good.price)
 }
 }
+
+
+let products  = document.getElementById('products');
+let btn_all = document.getElementById('btn_all');
+let btn_dress = document.getElementById('btn_dress');
+let btn_top = document.getElementById('btn_top');
+let btn_sweater= document.getElementById('btn_sweater');
+let btn_jackets = document.getElementById('btn_jackets');
+
+
+/* function showDresses */
+
+btn_dress.addEventListener('click', showDresses);
+function showDresses(){
+	let cards = document.querySelectorAll('.card');
+	for(let card of cards){
+		if(card.innerHTML.includes('Dress')){
+			card.style.display = 'block';
+			products.style.height = '1100px';
+		}else{
+			card.style.display = 'none';
+		}
+	}
+}
+
+
+/* function showTops */
+
+btn_top.addEventListener('click', showTops);
+function showTops(){
+	let cards = document.querySelectorAll('.card');
+	for(let card of cards){
+		if(card.innerHTML.includes('Top')){
+			card.style.display = 'block';
+			products.style.height = '1100px';
+		}else{
+			card.style.display = 'none';
+		}
+	}
+}
+
+
+
+/* function showSweaters */
+
+btn_sweater.addEventListener('click', showSweaters);
+function showSweaters(){
+	let cards = document.querySelectorAll('.card');
+	for(let card of cards){
+		if(card.innerHTML.includes('Sweater')){
+			card.style.display = 'block';
+			products.style.height = '1100px';
+		}else{
+			card.style.display = 'none';
+		}
+	}
+}
+
+
+
+/* function showJackets */
+
+btn_jackets.addEventListener('click', showJackets);
+function showJackets(){
+	let cards = document.querySelectorAll('.card');
+	for(let card of cards){
+		if(card.innerHTML.includes('Jacket')){
+			card.style.display = 'block';
+			products.style.height = '1100px';
+		}else{
+			card.style.display = 'none';
+		}
+	}
+}
+
+
+
+/* function showAll */
+
+btn_all.addEventListener('click', showAll);
+function showAll(){
+	let cards = document.querySelectorAll('.card');
+	for(let card of cards){
+			card.style.display = 'block';
+			products.style.height = '1850px';
+		}
+}
